ba1b5b03c14f4002afd37335ec2dfaf799a040e3
Senthilkumar Avanasiappan <savanasiappan@containerstore.com>
2023-11-30 21:45:44 +0530

    header code added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import React from 'react'; // Must be imported for webpack to work
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. done 123
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="headerApp">
+      <div>Header</div>
     </div>
   );
 }
 
-export default App;
+export default App; 
